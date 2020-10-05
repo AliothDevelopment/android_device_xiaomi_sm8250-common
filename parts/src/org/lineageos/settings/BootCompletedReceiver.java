a5e84491ea930502d6eea2811fc41d7368dbb068
dataoutputstream <mb150996@gmail.com>
2020-10-05 13:25:27 +0200

    sm8250-common: Add touchfeature impl of dt2w

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,17 +22,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.provider.Settings;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-
 import org.lineageos.settings.doze.DozeUtils;
 
 import org.lineageos.settings.fod.FodUtils;
+import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
-
+    public static final String SHAREDD2TW = "sharadeD2TW";
+    private ITouchFeature mTouchFeature;
     @Override
     public void onReceive(final Context context, Intent intent) {
      try {
@@ -49,7 +51,14 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             // Ignore
         }
 
-
+        //Micro-Service to restore sata of dt2w on reboot
+        SharedPreferences prefs = context.getSharedPreferences(SHAREDD2TW, Context.MODE_PRIVATE); 
+        try {
+            mTouchFeature = ITouchFeature.getService();
+            mTouchFeature.setTouchMode(14,prefs.getInt(SHAREDD2TW, 1));
+        } catch (Exception e) {
+            // Do nothing
+        }
 
     
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
