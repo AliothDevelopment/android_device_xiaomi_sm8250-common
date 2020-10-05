487561e4c1dcaa196d4ca349ea586d2dd7080d62
dataoutputstream <mb150996@gmail.com>
2020-10-05 13:17:58 +0200

    sm8250-common: Fod Service and hz menu from Picasso
    Credits @PeterCxy and @TheSarastic

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -21,8 +21,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.provider.Settings;
+import android.content.pm.PackageManager;
 
 import org.lineageos.settings.doze.DozeUtils;
+
+import org.lineageos.settings.fod.FodUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -31,7 +35,25 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+     try {
+            // We need to reset this setting to trigger an update in display service
+            final float refreshRate = Settings.System.getFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, 90.0f);
+            Thread.sleep(500);
+            Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, 90.0f);
+            Thread.sleep(500);
+            Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, refreshRate);
+        } catch (Exception e) {
+            // Ignore
+        }
+
+
+
+    
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
+        FodUtils.startService(context);
     }
 }
