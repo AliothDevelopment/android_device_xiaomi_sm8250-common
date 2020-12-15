87680b246c4027198ceac6d68e60b53ce6aacac6
dataoutputstream <mb150996@gmail.com>
2020-12-15 13:27:25 +0100

    sm8250-common: ContextHub hal

--- kona.mk
+++ kona.mk
@@ -61,6 +61,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/net.sourceforge.opencamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/net.sourceforge.opencamera.xml
+    
+# Context hub HAL
+PRODUCT_PACKAGES += \
+    android.hardware.contexthub@1.0-impl.generic \
+    android.hardware.contexthub@1.0-service
 
 # Common init scripts
 PRODUCT_PACKAGES += \
