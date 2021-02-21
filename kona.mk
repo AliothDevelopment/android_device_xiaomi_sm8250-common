4c3a835763b984d68e5e2605db4fb3e7ea42f1fd
dataoutputstream <mb150996@gmail.com>
2021-02-22 00:14:53 +0100

    sm8250-common: More Descendant-ify

--- kona.mk
+++ kona.mk
@@ -157,7 +157,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-descendant
     
 # Overlays - override vendor ones
 PRODUCT_PACKAGES += \
