471913c3f7bdc47dcbf022580c9f043e4de7cb48
dataoutputstream <mb150996@gmail.com>
2021-03-03 12:28:14 +0100

    sm8250-common: Remove vendor sepolicy we don't build vendor at this moment, this  sepolicy won't be never loaded so cut for now.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,7 +182,6 @@ SELINUX_IGNORE_NEVERALLOWS := true
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private-vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
-BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 include device/qcom/sepolicy/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
