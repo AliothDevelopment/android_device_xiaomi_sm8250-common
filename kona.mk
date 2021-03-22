a33daaa772661e5fda822c642af23e373e7f88d3
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2021-03-22 15:58:08 +0100

    sm8250-common: Import powerhint.json from android-11.0.0_r32
    Change-Id: I552870c130ceaac4d20322acf6d1da22ad5017ec

--- kona.mk
+++ kona.mk
@@ -285,6 +285,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel
