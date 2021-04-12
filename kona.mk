b7932c7e7d142e01d960899627ea1386134fcb28
MB <mb150996@gmail.com>
2021-04-12 20:29:40 +0200

    sm8250-common: build HIDL packages

--- kona.mk
+++ kona.mk
@@ -226,9 +226,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
-    libhidltransport \
-    libhwbinder \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0\
+    android.hidl.manager@1.0.vendor\
     libhwbinder.vendor
 
 # HotwordEnrollement app permissions
