735f741ab1ea1a6324e167d085cb56836486bcba
MB <mb150996@gmail.com>
2021-04-24 16:27:13 +0200

    sm8250-common: Correct Guards

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter apollo cas cmi lmi umi,$(TARGET_DEVICE)),)
+ifneq ($(filter alioth thyme,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
