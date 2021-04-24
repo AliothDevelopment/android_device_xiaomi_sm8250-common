9627f7bc0bfb2737afc70f30e2477f74ee5e11ae
MB <mb150996@gmail.com>
2021-04-24 16:27:13 +0200

    sm8250-common: Correct Guards

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -29,7 +29,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
 
 # Copyright headers and guards
-write_headers "umi cmi lmi"
+write_headers "alioth thyme"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
