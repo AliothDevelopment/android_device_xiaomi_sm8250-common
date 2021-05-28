8224b5d9ddce03b33ebad109f91d3f9a846633c7
MB <mb150996@gmail.com>
2021-05-28 19:34:25 +0200

    sm8250-common: Already added into proprietary files

--- kona.mk
+++ kona.mk
@@ -159,15 +159,7 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     libipanat \
     liboffloadhal
-    
-# IRQ
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
-    
-# IRSC
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config \
-    
+        
 # Libshim
 #PRODUCT_PACKAGES += \
     libhidlbase_shim
