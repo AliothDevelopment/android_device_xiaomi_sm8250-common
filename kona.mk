c06fde961a34369a5a816d18bb93505c41655a77
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2021-06-14 07:26:35 +0300

    sm8250-common: Enable freeform windows Change-Id: I94f4f6661c493a7e2606046e669f3528e5bde176
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- kona.mk
+++ kona.mk
@@ -127,6 +127,10 @@ PRODUCT_COPY_FILES += \
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
+	
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
 # Health
 PRODUCT_PACKAGES += \
