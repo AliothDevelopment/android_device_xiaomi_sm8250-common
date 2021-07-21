efef5652c7cd761a2addeeccd7a87555ab888c8c
MB <mb150996@gmail.com>
2021-07-21 17:00:17 +0200

    sm8250-common: fix-up vibrator build

--- kona.mk
+++ kona.mk
@@ -394,7 +394,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_kona
     
 #VNDK
 PRODUCT_PACKAGES += \
