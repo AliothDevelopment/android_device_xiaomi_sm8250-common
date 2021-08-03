c60a530bd1550f42f03c0e5698635e04a03a3c0b
MB <mb150996@gmail.com>
2021-08-03 18:37:51 +0200

    sm8250-common: Build AEX

--- kona.mk
+++ kona.mk
@@ -225,7 +225,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aex
     
 # OTA
 PRODUCT_HOST_PACKAGES += \
