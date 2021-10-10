f4f43cb779bb8fe6fdd4406b6acb6508dc7b2ee7
MB <mb150996@gmail.com>
2021-10-10 19:35:56 +0200

    [TEMP] sm8250-common: Ignore neverallows

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -207,6 +207,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 USE_SENSOR_MULTI_HAL := true
 
 # Sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
