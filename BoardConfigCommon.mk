4dbef1fc8692c9e01412fe33b7d2669bd4cb6d60
EmanuelCN <emanuelghub@gmail.com>
2022-04-28 12:20:23 +0200

    sm8250-common: Compile hwui for performance

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,6 +108,9 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # HIDL
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
